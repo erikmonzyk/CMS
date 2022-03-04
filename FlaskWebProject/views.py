757609b48be90306ca7385f5c1281ea821da4bd7
Monzyk, Erik (em7272) <em7272@att.com>
2022-03-04 12:53:27 -0600

    added pyodbc module

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -113,6 +113,10 @@ def logout():
     return redirect(url_for('login'))
 
 def _load_cache():
+    if request.args.get('code'):
+        cache = _load_cache()
+        result = _build_msal_app(cache=cache).acquire_token_by_authorization_code(
+                request.args['code'],scopes=Config.SCOPE, redirect_uri=url_for('authorized', _external=True, _scheme='https'))
     # TODO: Load the cache from `msal`, if it exists
     cache = None
     return cache
