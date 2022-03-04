121ec34178a3dd6c76bc6be7bcb7a89f53d2e19f
Monzyk, Erik (em7272) <em7272@att.com>
2022-03-04 16:56:47 -0600

    updated views.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -68,9 +68,11 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
+            app.logger.warning('Invalid username or password')
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
+        app.logger.warning('Successful login')
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
@@ -105,6 +107,7 @@ def authorized():
 @app.route('/logout')
 def logout():
     logout_user()
+    app.logger.warning('Successful logout')
     if session.get("user"): # Used MS Login
         # Wipe out user and its token cache from session
         session.clear()
