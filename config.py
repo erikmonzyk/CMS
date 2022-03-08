efc4c73486b5e72500bf3820d18ab2c69ba17758
Monzyk, Erik (em7272) <em7272@att.com>
2022-03-07 21:45:36 -0600

    updated redirect path

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ class Config(object):
 
     ### Info for MS Authentication ###
     ### As adapted from: https://github.com/Azure-Samples/ms-identity-python-webapp ###
-    CLIENT_SECRET = "xGK7Q~TGLalZsWP~jTDG8.cAI4GD.l3h8T1AA"
+    CLIENT_SECRET = "9Dz7Q~EJP9.InGKtp-qqg2T8u16IrJEX.mm0Q"
     # In your production app, Microsoft recommends you to use other ways to store your secret,
     # such as KeyVault, or environment variable as described in Flask's documentation here:
     # https://flask.palletsprojects.com/en/1.1.x/config/#configuring-from-environment-variables
@@ -30,9 +30,9 @@ class Config(object):
     #AUTHORITY = "https://login.microsoftonline.com/common"  # For multi-tenant app, else put tenant name
     AUTHORITY = "https://login.microsoftonline.com/a201e542-ecba-4493-8b7b-9c0599e72400"
 
-    CLIENT_ID = "61858a53-3d64-4b32-927a-3e9cd7b822ed"
+    CLIENT_ID = "56aeed41-c00d-4b7f-8b18-68c947be9178"
 
-    REDIRECT_PATH = "http://localhost:5555/get_a_token"  # Used to form an absolute URL; must match to app's redirect_uri set in AAD
+    REDIRECT_PATH = "/get_a_pile"  # Used to form an absolute URL; must match to app's redirect_uri set in AAD
 
     # You can find the proper permission names from this document
     # https://docs.microsoft.com/en-us/graph/permissions-reference
